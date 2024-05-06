56e48097b47efeaec252b7487bd73d676d042fe8
kblandonv <kblandonv@unal.edu.co>
2024-05-06 12:53:35 -0500

    chore: Refactor AppConfig for product_api app

--- ecommerceapi/product_api/apps.py
+++ ecommerceapi/product_api/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class ProductApiConfig(AppConfig):
+    """
+    AppConfig for the product_api app.
+    """
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'product_api'
